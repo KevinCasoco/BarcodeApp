0d30cd7cc42be22df0e0747cc7ec5bd8a371866d
Andrei Kevin D. Casoco <andreikevincasoco@gmail.com>
2026-02-24 21:06:31 +0800

    Add project files.

---
+++ BarcodeApp/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace BarcodeApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEAN13 = new TextBox();
+            btnGenerate = new Button();
+            pictureBoxBarcode = new PictureBox();
+            txtModelNumber = new TextBox();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            cmbModule = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxBarcode).BeginInit();
+            SuspendLayout();
+            // 
+            // txtEAN13
+            // 
+            txtEAN13.Location = new Point(345, 38);
+            txtEAN13.Name = "txtEAN13";
+            txtEAN13.Size = new Size(163, 23);
+            txtEAN13.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(580, 80);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(92, 23);
+            btnGenerate.TabIndex = 1;
+            btnGenerate.Text = "generate me";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // pictureBoxBarcode
+            // 
+            pictureBoxBarcode.Location = new Point(107, 202);
+            pictureBoxBarcode.Name = "pictureBoxBarcode";
+            pictureBoxBarcode.Size = new Size(682, 322);
+            pictureBoxBarcode.TabIndex = 2;
+            pictureBoxBarcode.TabStop = false;
+            // 
+            // txtModelNumber
+            // 
+            txtModelNumber.Location = new Point(345, 137);
+            txtModelNumber.Name = "txtModelNumber";
+            txtModelNumber.Size = new Size(163, 23);
+            txtModelNumber.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(230, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 6;
+            label1.Text = "BARCODE:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(230, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 15);
+            label3.TabIndex = 8;
+            label3.Text = "MODULE:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(230, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 15);
+            label4.TabIndex = 9;
+            label4.Text = "MODEL:";
+            // 
+            // cmbModule
+            // 
+            cmbModule.FormattingEnabled = true;
+            cmbModule.Items.AddRange(new object[] { "5081", "5628", "3523", "5662" });
+            cmbModule.Location = new Point(345, 97);
+            cmbModule.Name = "cmbModule";
+            cmbModule.Size = new Size(163, 23);
+            cmbModule.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(985, 623);
+            Controls.Add(cmbModule);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(txtModelNumber);
+            Controls.Add(pictureBoxBarcode);
+            Controls.Add(btnGenerate);
+            Controls.Add(txtEAN13);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBoxBarcode).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEAN13;
+        private Button btnGenerate;
+        private PictureBox pictureBoxBarcode;
+        private TextBox txtModelNumber;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private ComboBox cmbModule;
+    }
+}
---
+++ BarcodeApp/Form1.cs
@@ -0,0 +1,134 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarcodeApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            const string DI = "DI";
+            string modelNumber = txtModelNumber.Text.Trim();
+            string module = cmbModule.Text.Trim();
+            string ean13 = txtEAN13.Text.Trim();
+
+            // strict 13 digits only
+            if (ean13.Length != 13)
+            {
+                MessageBox.Show("EAN-13 must have exactly 13 digits.",
+                    "Invalid Input",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            // strict numbers only
+            if (!ean13.All(char.IsDigit))
+            {
+                MessageBox.Show("EAN-13 must contain digits only.",
+                    "Invalid Input",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Format barcode ID: 1234567 12345 1
+            string formattedEAN =
+                $"{ean13.Substring(0, 7)} {ean13.Substring(7, 5)} {ean13.Substring(12, 1)}";
+
+            try
+            {
+                Barcode barcode = new Barcode();
+                barcode.IncludeLabel = false; // remove default text
+                barcode.Alignment = AlignmentPositions.CENTER;
+
+                Image barcodeImage =
+                    barcode.Encode(TYPE.EAN13, ean13, Color.Black, Color.White, 500, 180);
+
+                Bitmap finalImage =
+                    new Bitmap(barcodeImage.Width + 40, barcodeImage.Height + 150);
+
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    g.Clear(Color.White);
+
+                    // Top Left (Model Name)
+                    using (Font font = new Font("OCR-B", 20, FontStyle.Bold))
+                    {
+                        g.DrawString(modelNumber, font, Brushes.Black, new PointF(10, 5));
+                    }
+
+                    // Box Module
+                    using (Font font = new Font("OCR-B", 20, FontStyle.Bold))
+                    {
+                        SizeF textSize = g.MeasureString(module, font);
+                        RectangleF rect =
+                            new RectangleF(10, 35, textSize.Width + 10, textSize.Height + 5);
+
+                        g.DrawRectangle(Pens.Black, rect.X, rect.Y, rect.Width, rect.Height);
+                        g.DrawString(module, font, Brushes.Black,
+                            new PointF(rect.X + 5, rect.Y + 2));
+                    }
+
+                    // Top Right (DI)
+                    using (Font font = new Font("OCR-B", 20, FontStyle.Bold))
+                    {
+                        SizeF textSize = g.MeasureString(DI, font);
+                        g.DrawString(DI, font, Brushes.Black,
+                            new PointF(finalImage.Width - textSize.Width - 10, 35));
+                    }
+
+                    // Draw Barcode
+                    int barcodeX = 20;
+                    int barcodeY = 80;
+
+                    g.DrawImage(barcodeImage, new Point(barcodeX, barcodeY));
+
+                    using (Font font = new Font("OCR-B", 20, FontStyle.Bold))
+                    {
+                        float textY = barcodeY + barcodeImage.Height + 5;
+
+                        char[] chars = formattedEAN.ToCharArray();
+                        int charCount = chars.Length;
+
+                        float totalWidth = barcodeImage.Width;
+                        float firstCharWidth = g.MeasureString(chars[0].ToString(), font).Width;
+                        float lastCharWidth = g.MeasureString(chars[charCount - 1].ToString(), font).Width;
+                        float usableWidth = totalWidth - lastCharWidth;
+                        float spacing = usableWidth / (charCount - 1);
+
+                        for (int i = 0; i < charCount; i++)
+                        {
+                            float x = barcodeX + (spacing * i);
+                            g.DrawString(chars[i].ToString(), font, Brushes.Black, x, textY);
+                        }
+                    }
+                }
+
+                pictureBoxBarcode.Image = finalImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating barcode: " + ex.Message);
+            }
+        }
+    }
+}
